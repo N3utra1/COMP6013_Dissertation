88e652c75826528f1d6ce96f0b63d9e104ee8ed9
William <william.riddell@protonmail.com>
2024-03-29 23:14:01 +0000

    WIP still

--- codebase/cnn/calculate_metrics.py
+++ codebase/cnn/calculate_metrics.py
@@ -14,10 +14,11 @@ from sklearn.metrics import classification_report, accuracy_score
 
 
 import control
-import cnn.cnn.generate_datasets
+from cnn.cnn import Generator
 
 class calculate_metrics:
     def __init__(self):
+        self.stft_path = control.stft_extraction_path
         models = glob.glob(os.path.join(control.model_save_path, "*", "*.keras"))
         results = glob.glob(os.path.join(control.model_save_path, "*", "*.results"))
         for model in models:
@@ -26,12 +27,12 @@ class calculate_metrics:
                 continue
 
             print(f"$$ calculating model metrics for {model}")
-            self.train_generator, self.test_generator = cnn.cnn.generate_datasets()
 
+            self.generator = self.get_generator()
             self.model = load_model(model)
 
             y_true = self.test_generator.classes
-            y_pred = self.model.predict(self.test_generator, verbose=0)
+            y_pred = self.model.predict(self.generator, verbose=0)
             y_pred = np.argmax(y_pred, axis=1)
 
             cm = confusion_matrix(y_true, y_pred)
@@ -42,3 +43,29 @@ class calculate_metrics:
             print(f"Accuracy: {accuracy}")
             print("Classification Report:")
             print(classification_report(y_true, y_pred))
+
+
+
+
+    def get_generator(self):
+        # generate one hot encoded matrix
+        classes = ["interictal", "preictal", "ictal"]
+        integer_mapping = {x: i for i,x in enumerate(classes)}
+        labels = [integer_mapping[c] for c in classes]
+        one_hot_matrix = to_categorical(labels, num_classes=len(classes))
+
+        mode = type(control.target)
+        if  mode == type([]): target_subjects = control.target # already an array
+        elif mode == type(bool): target_subjects = [path.split(os.sep)[-1] for path in glob.glob(self.stft_path+os.sep+"*")] # get all subject folders
+        elif mode == type(""): target_subjects = [control.target] # only a string
+        else: raise ValueError
+
+        file_paths = []
+        labels = []
+        for subject in target_subjects:
+            file_paths += glob.glob(os.path.join(self.stft_path, subject, "*", "*.npy"))
+            labels += [one_hot_matrix[classes.index(path.split(os.sep)[-2])] for path in file_paths]
+
+        return Generator(file_paths, labels, self.batch_size)
+        
+
