c6169991826e8c8082e8d8d0d428a8539f6463c5
William <william.riddell@protonmail.com>
2024-03-30 20:41:56 +0000

    added set memory growth

--- codebase/gui/init.py
+++ codebase/gui/init.py
@@ -12,7 +12,11 @@ import mne
 import tensorflow as tf
 
 # helps with OOM errors:
-
+try:
+    physical_devices = tf.config.list_physical_devices('GPU') 
+    if physical_devices: tf.config.experimental.set_memory_growth(physical_devices[0], True)
+except:
+    pass
 
 # simulating controls the model
 simulating = False
